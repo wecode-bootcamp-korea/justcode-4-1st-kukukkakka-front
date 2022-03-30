b8b4945667be249c7275b78bf64e6b4f4dac9af8
alicia-mkkim <alicia.mkkim@gmail.com>
2022-03-30 20:49:36 +0900

    [ADD]: cart layout 상품정보까지 진행됨

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import styles from '../Cart/Cart.module.scss';
 
 function Cart() {
-  return <div />;
+  return (
+    <section className={styles.cartSection}>
+      <h1 className={styles.title}>장바구니</h1>
+      <div className={styles.cartHead}>
+        <input type="checkbox" className={styles.checkbox} />
+        <p className={styles.info}>상품정보</p>
+        <p className={styles.option}>추가상품</p>
+        <p className={styles.price}>합계금액</p>
+      </div>
+      <div className={styles.cartCenter}>
+        <input type="checkbox" className={styles.checkbox} />
+        <div className={styles.info}>
+          <div className={styles.productImg}>
+            <img src="" alt="product" />
+          </div>
+          <div className={styles.productDetail}>
+            <h2 className={styles.name}>플라워 샤워 에디션</h2>
+            <p className={styles.dueDate}>수령일: 2022-04-01</p>
+            <span className={styles.price}>17,000원</span>
+            <div className={styles.quantityBox}>
+              <button>-</button>
+              <span>1</span>
+              <button className={styles.plus}>+</button>
+            </div>
+            <button className={styles.delete}>X</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Cart;
