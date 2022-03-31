394529c602ce276276719ec716f5aef56d97ffdb
dami kim <tmkim@sunnywt.com>
2022-03-31 15:53:23 +0900

    [Add] login e.target.value까지 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Login.module.scss';
 
 function Login() {
+  // const [modal, setModal] = useState(false);
+
+  // const openUp = () => {
+  //   setModal(true);
+  // };
+  // const onClose = () => {
+  //   setModal(false);
+  // };
+
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const navigate = useNavigate();
+  const loginSuccess = () => {
+    navigate('/');
+  };
+
+  const idInput = e => {
+    setId(e.target.value);
+  };
+  const pwInput = e => {
+    setPw(e.target.value);
+  };
+
+  const preparing = () => {
+    alert('준비중입니다! 조금만 기다려주세요');
+  };
+
   return (
     <section className={styles.wrap}>
       <header className={styles.header}> 로그인 </header>
@@ -9,8 +37,14 @@ function Login() {
         className={styles.idBox}
         placeholder="아이디(이메일)"
         type="text"
+        onChange={idInput}
       />
-      <input className={styles.pwBox} placeholder="비밀번호" type="password" />
+      <input
+        className={styles.pwBox}
+        placeholder="비밀번호"
+        type="password"
+        onChange={pwInput}
+      />
       <button className={styles.loginBtn}>로그인</button>
       <section className={styles.searchLoginInfo}>
         <span className={styles.searchId}>아이디 찾기</span>
@@ -18,7 +52,7 @@ function Login() {
       </section>
       <hr className={styles.line} />
       <span className={styles.noticeText}> SNS계정으로 간편 로그인</span>
-      <section className={styles.snsIcon}>
+      <section className={styles.snsIcon} onClick={preparing}>
         <img
           className={styles.facebookIcon}
           src="https://ifh.cc/g/4mMCRP.png"
---
+++ src/pages/Login/Modal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Modal() {
+  return <div>꾸꾸까까가 열심히 준비중입니다! 조금만 기다려주세요!</div>;
+}
+
+export default Modal;
