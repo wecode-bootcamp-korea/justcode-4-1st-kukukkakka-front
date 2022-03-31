c1478c6f1c7717e021fc544300a66129b2236d38
xxziiko <xxziiko@gmail.com>
2022-03-31 18:59:25 +0900

    [Add]: OptionBtn 기능 구현 중 임시저장_2

--- src/pages/Detail/OptionBtn.js
+++ src/pages/Detail/OptionBtn.js
@@ -2,17 +2,29 @@ import React, { useState } from 'react';
 import style from './OptionBtn.module.scss';
 
 function OptionBtn() {
+  const [hidden, setHidden] = useState({ display: 'none' });
+
+  function onClickOptions() {
+    hidden === { display: 'block' }
+      ? setHidden({ display: 'none' })
+      : setHidden({ display: 'block' });
+  }
+  console.log('hidden : ', hidden);
   return (
     <div>
-      <div className={style.optionBtnBox}>
-        <button className={style.optionBtn}>
+      <div className={style.option_btn_box}>
+        <button className={style.optionBtn} onClick={onClickOptions}>
           <div>함께하면 좋은 추천상품</div>
           <i className={style.btnDown}>⌵</i>
         </button>
-        <button className={style.hidden_option_btn}>롱 모던 베이직 화병</button>
-        <button className={style.hidden_option_btn}>롱 모던 베이직 화병</button>
-        <button className={style.hidden_option_btn}>롱 모던 베이직 화병</button>
-        <button className={style.hidden_option_btn}>롱 모던 베이직 화병</button>
+        <ul style={hidden}>
+          <li>롱 모던 베이직 화병</li>
+          <li>4900원</li>
+        </ul>
+        <ul style={hidden}>
+          <li>롱 모던 베이직 화병</li>
+          <li>4900원</li>
+        </ul>
       </div>
     </div>
   );
