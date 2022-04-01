bdcc53a16736ed627ccd344c54fd90f59bddb911
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-01 17:40:07 +0900

    [Add] : 유저가 찾은 키워드를 백으로 보내주기 추가 --- 백엔드API 구현전

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -7,6 +7,7 @@ function List() {
     productList: [],
   });
 
+  // const [lowerPrice, setLowerPrice] = useState([]);
   const [inputs, setInputs] = useState('');
 
   useEffect(() => {
@@ -18,14 +19,34 @@ function List() {
       });
   }, []);
 
+  //유저가 찾은 키워드를 백엔드에 보내주기
+
+  const sendKeywords = () => {
+    fetch('/keywords', {
+      method: 'post',
+      body: JSON.stringify({
+        keyword: inputs,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => console.log({ message: result }));
+  };
+
   const inputValue = e => {
     e.preventDefault();
     if (e.key === 'Enter') setInputs(e.target.value);
   };
 
-  const filterWords = lists.productList.filter(list =>
-    list.name.includes(inputs)
-  );
+  // const filterWords = lists.productList.filter(list =>
+  //   list.name.includes(inputs)
+  // );
+  const getPrice = lists.productList.sort((a, b) => a.price - b.price);
+  console.log(getPrice);
+  console.log('list:', lists.productList);
+  // const sortByPrice = () => {
+  //   console.log('clicked');
+  //   return setLowerPrice(getPrice);
+  // };
   return (
     <>
       <section className={styles.listBanner}>
@@ -45,7 +66,7 @@ function List() {
               onChange={inputValue}
               onKeyUp={inputValue}
             />
-            <div className={styles.icons}>
+            <div className={styles.icons} onClick={sendKeywords}>
               <AiOutlineSearch size="1.5em" color="#ddd" />
             </div>
           </div>
@@ -53,13 +74,13 @@ function List() {
           <div className={styles.filterBox}>
             <ul>
               <li>사이즈순</li>
-              <li>가격 낮은 순</li>
+              <li onClick={() => {}}>가격 낮은 순</li>
             </ul>
           </div>
         </div>
       </section>
       <div className={styles.listContainer}>
-        {filterWords.map(list => (
+        {lists.productList.map(list => (
           <ListCard list={list} key={list.id} />
         ))}
       </div>
