644fc92f88dfb4209e81891eab6c9a30a971dfb3
dami kim <tmkim@sunnywt.com>
2022-04-04 13:54:47 +0900

    [Modify] useState 정리중

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -4,23 +4,47 @@ import { useNavigate } from 'react-router-dom';
 import stylse from './Signup.module.scss';
 
 function Signup() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordAgain, setPasswordAgain] = useState('');
-  const [username, setUsername] = useState('');
-  const [gender, setGender] = useState('female');
-  const [checked, setChecked] = useState(false);
+  // input창에 기입되는 info 상태
+  const [inputs, setInputs] = useState([
+    {
+      id: '',
+      password: '',
+      passwordAgain: '',
+      username: '',
+      gender: 'female',
+      checked: false,
+    },
+  ]);
 
-  const [errtext, setErrtext] = useState('');
-  const [idCheck, setIdCheck] = useState(false);
-  const [textcolor_id, setTextcolor_id] = useState(false);
+  console.log('할당전 : ', inputs, inputs.gender);
 
-  const [pwErrtext, setPwErrtext] = useState('');
-  const [textcolor_pw, setTextcolor_pw] = useState(false);
+  const { id, password, passwordAgain, username, gender, checked } = inputs;
+  console.log('어떻게 찍히나 볼까 :', inputs, inputs.gender, gender);
+  // password, passwordAgain, username, gender, checked
 
-  const [pwAgainErrtext, setPwAgainErrtext] = useState('');
-  const [textcolor_pwagain, setTextcolor_pwagain] = useState(false);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [passwordAgain, setPasswordAgain] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [gender, setGender] = useState('female');
+  // const [checked, setChecked] = useState(false);
 
+  // input창 내용에 맞게 보여지는 errtext + color
+  const [errtext, setErrtext] = useState({
+    text: '',
+    color: false,
+    idCheck: false,
+  });
+  const [pwErrtext, setPwErrtext] = useState({
+    text: '',
+    color: false,
+  });
+  const [pwAgainErrtext, setPwAgainErrtext] = useState({
+    text: '',
+    color: false,
+  });
+
+  // signup button 활성화를 위한 기입상태 확인
   const [isPassed, setIsPassed] = useState(false);
   const navigate = useNavigate();
 
@@ -30,84 +54,94 @@ function Signup() {
 
   console.log(isPassed);
 
+  // 회원가입 --> POST fetch로 로직 옮겨야함
   const signupSuccess = () => {
     alert('회원가입을 축하드립니다!');
     navigate('/main');
   };
-  const policyAgree = () => {
-    setChecked(!checked);
-  };
-  const genderChoice = (e, data) => {
-    setGender(data);
-  };
 
+  //이용약관 동의여부 로직
+  const policyAgree = () => {
+    setInputs((checked = !checked));
+  };
+  // 성별정보 선택 데이터 로직
+  const genderChoice = (e, data) => {
+    setInputs((gender = data));
+  };
+  console.log('inputs는 :', inputs);
+  console.log('gender는: ', gender);
+
+  //
   const emailHandler = e => {
-    setEmail(e.target.value);
+    setInputs((id = e.target.value));
+    // setEmail(e.target.value);
     if (e.target.value === '') {
-      setErrtext('');
-      setTextcolor_id(false);
-      setIdCheck(false);
-    } else if (!email.includes('@')) {
-      setErrtext('아이디는 이메일형식 이어야 합니다.');
-      setTextcolor_id(false);
-      setIdCheck(false);
-    } else if (email.includes('@')) {
-      setErrtext('형식에 맞는 아이디입니다.');
-      setTextcolor_id(true);
-      setIdCheck(true);
+      setErrtext({ text: '', color: false, idCheck: false });
+    } else if (!id.includes('@')) {
+      setErrtext({
+        text: '아이디는 이메일형식 이어야 합니다.',
+        color: false,
+        idCheck: false,
+      });
+    } else if (id.includes('@')) {
+      setErrtext({
+        text: '형식에 맞는 아이디입니다.',
+        color: true,
+        idCheck: true,
+      });
     }
   };
+  console.log(inputs.id);
   const passwordHandler = e => {
-    setPassword(e.target.value);
+    setInputs((inputs.password = e.target.value));
     let regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/;
     if (e.target.value === '') {
-      setPwErrtext('');
-      setTextcolor_pw(false);
+      setPwErrtext({ text: '', color: false });
     } else if (password.length < 8) {
-      setPwErrtext('비밀번호는 8자리 이상이어야 합니다.');
-      setTextcolor_pw(false);
+      setPwErrtext({
+        text: '비밀번호는 8자리 이상이어야 합니다.',
+        color: false,
+      });
     } else if (password.length >= 8 && regExp.test(password)) {
-      setPwErrtext('사용가능한 비밀번호 입니다');
-      setTextcolor_pw(true);
+      setPwErrtext({ text: '사용가능한 비밀번호 입니다', color: true });
     } else if (password.length >= 8 && !regExp.test(password)) {
-      setPwErrtext('영문, 숫자가 모두 포함되어야 합니다');
-      setTextcolor_pw(false);
+      setPwErrtext({
+        text: '영문, 숫자가 모두 포함되어야 합니다',
+        color: false,
+      });
     }
   };
 
   const passwordAgainHandler = e => {
-    setPasswordAgain(e.target.value);
+    setInputs((inputs.passwordAgain = e.target.value));
     if (e.target.value === '') {
-      setPwAgainErrtext('');
-      setTextcolor_pwagain(false);
+      setPwAgainErrtext({ text: '', color: false });
     } else if (e.target.value !== password) {
-      setPwAgainErrtext('비밀번호가 일치하지 않습니다.');
-      setTextcolor_pwagain(false);
+      setPwAgainErrtext({
+        text: '비밀번호가 일치하지 않습니다.',
+        color: false,
+      });
     } else if (e.target.value === password) {
-      setPwAgainErrtext('비밀번호가 일치합니다');
-      setTextcolor_pwagain(true);
+      setPwAgainErrtext({ text: '비밀번호가 일치합니다', color: true });
     }
   };
 
   const usernameHandler = e => {
-    setUsername(e.target.value);
+    setInputs((inputs.username = e.target.value));
   };
 
-  const isPassedSignup = () => {
-    if (
-      textcolor_id === true &&
-      textcolor_pw === true &&
-      textcolor_pwagain === true &&
-      username !== '' &&
-      checked === true
-    ) {
-      setIsPassed(true);
-    } else setIsPassed(false);
-  };
+  // const isPassedSignup = () => {
+  //   if (
+  //     textcolor_id === true &&
+  //     textcolor_pw === true &&
+  //     textcolor_pwagain === true &&
+  //     username !== '' &&
+  //     checked === true
+  //   ) {
+  //     setIsPassed(true);
+  //   } else setIsPassed(false);
+  // };
 
-  console.log(textcolor_id);
-  console.log(textcolor_pw);
-  console.log(textcolor_pwagain);
   console.log(username !== '');
   console.log(checked);
   console.log('ispassed값 : ', isPassed);
@@ -128,7 +162,7 @@ function Signup() {
           />
           <button
             className={
-              idCheck
+              errtext.idCheck
                 ? stylse.idDuplicateCheck_true
                 : stylse.idDuplicateCheck_false
             }
@@ -138,10 +172,10 @@ function Signup() {
         </div>
         <div
           className={
-            textcolor_id ? stylse.validateGuide : stylse.unvalidateGuide
+            errtext.color ? stylse.validateGuide : stylse.unvalidateGuide
           }
         >
-          {errtext}
+          {errtext.text}
         </div>
         <div className={stylse.setWrapper}>
           <span>비밀번호</span>
@@ -154,10 +188,10 @@ function Signup() {
         </div>
         <div
           className={
-            textcolor_pw ? stylse.validateGuide : stylse.unvalidateGuide
+            pwErrtext.color ? stylse.validateGuide : stylse.unvalidateGuide
           }
         >
-          {pwErrtext}
+          {pwErrtext.text}
         </div>
         <div className={stylse.setWrapper}>
           <span> 비밀번호 확인 </span>
@@ -169,11 +203,10 @@ function Signup() {
         </div>
         <div
           className={
-            textcolor_pwagain ? stylse.validateGuide : stylse.unvalidateGuide
+            pwAgainErrtext.color ? stylse.validateGuide : stylse.unvalidateGuide
           }
         >
-          {' '}
-          {pwAgainErrtext}{' '}
+          {pwAgainErrtext.text}
         </div>
         <div className={stylse.setWrapper}>
           <span>이름</span>
@@ -188,7 +221,7 @@ function Signup() {
           <section className={stylse.genderWrap}>
             <button
               className={
-                gender === 'female'
+                inputs.gender === 'female'
                   ? stylse.genderBtn_true
                   : stylse.genderBtn_false
               }
@@ -245,13 +278,13 @@ function Signup() {
           이용약관보기 · 개인정보처리방침 보기{' '}
         </section>
         <button
-          className={
-            isPassedSignup === true
-              ? stylse.signuoBtn_true
-              : stylse.signuoBtn_false
-          }
+          // className={
+          //   // isPassedSignup === true
+          //   //   ? stylse.signuoBtn_true
+          //   //   : stylse.signuoBtn_false
+          // }
           type="submit"
-          disabled={!isPassedSignup}
+          // disabled={!isPassedSignup}
           onClick={signupSuccess}
         >
           회원가입
