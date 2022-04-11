b392eabd291bc43269aa6afe5dfe48c00c1a4245
alicia-mkkim <alicia.mkkim@gmail.com>
2022-04-12 00:30:54 +0900

    [modify]: develop merge 전 커밋

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -10,10 +10,7 @@ function Cart() {
     fetch('http://localhost:8000/carts', {
       headers: {
         token: token,
-        headers: {
-          'Content-Type': 'application/json',
-          token: token,
-        },
+        'Content-Type': 'application/json',
       },
     })
       .then(res => res.json())
